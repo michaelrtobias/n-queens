1bc3ed146d96a3684f896202a3443d8ca4a4e2f5
Richard Ok <jaeok2017@u.northwestern.edu>
2020-08-26 22:15:03 -0500

    Added solvers pseudocode

--- src/solvers.js
+++ src/solvers.js
@@ -18,6 +18,19 @@
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
 
+  // number of rooks is n
+  // make nxn board
+  // call colConflicts and rowConflicts on every cell
+  // for i = 0 to i = length of board
+  //
+    // if both return false
+      // turn the cell value to 1
+  // return the board
+
+  // var newBoard = new Board(dimensions -> {n: x});
+  // var newBoard = new Board(board -> [[0, 0], [0, 0]]);
+  // start by putting a piece in top left corner
+
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
@@ -25,6 +38,8 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
+  // call find N rooks solution for every space
+  // if possible (somehow) don't double count solutions
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
