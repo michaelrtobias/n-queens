912ad30273d9dc6c66c021347295700617d114a3
Richard Ok <jaeok2017@u.northwestern.edu>
2020-08-27 18:26:10 -0500

    Completes NRooks

--- src/solvers.js
+++ src/solvers.js
@@ -16,31 +16,64 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  //debugger;
+  var solution = new Board({n: n});
 
   // number of rooks is n
+  var nRooks = n;
   // make nxn board
-  // call colConflicts and rowConflicts on every cell
+  var board = new Board({n: n});
+  var allRows = board.rows();
   // for i = 0 to i = length of board
-  //
-    // if both return false
-      // turn the cell value to 1
-  // return the board
+  for (var row = 0; row < board.get('n'); row++) {
+    for (var col = 0; col < board.get('n'); col++) {
+      allRows[row][col] = 1;
+      if (board.hasColConflictAt(col) || board.hasRowConflictAt(row)) {
+        allRows[row][col] = 0;
+      }
+    }
+  }
 
+  solution = board;
+  // call colConflicts and rowConflicts on every cell
   // var newBoard = new Board(dimensions -> {n: x});
   // var newBoard = new Board(board -> [[0, 0], [0, 0]]);
-  // start by putting a piece in top left corner
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
   // call find N rooks solution for every space
   // if possible (somehow) don't double count solutions
+  // (n-1)! possibilities
+  // use hasAnyRooksConflicts
+  var result = n;
 
+  // var factorial = function(num) {
+  //   if (num === 1 || num === 0) {                 // if n = 0: return 1
+  //     return 1;                                   // if n = 1: return 1
+  //   } else {
+  //     result = (result - 1) * factorial(num - 1); // if n = 2: 1 * factorial(0) = 1 * 1 = 1
+  //                                                 // if n = 3: 2 * factorial(2) = 2 * 1 = 2
+  //                                                 // if n = 4: 3 * factorial(3) = 3 * 2 = 6
+  //   }
+  // }
+
+  var factorial = function(num) {
+    if (num === 1 || num === 0) {                 // if n = 0: return 1
+      return 1;                                   // if n = 1: return 1
+    } else {
+      return result = num * factorial(num - 1);   // if n = 2: 2 * factorial(1) = 2 * 1 = 2
+                                                  // if n = 3: 3 * factorial(2) = 3 * 2 = 6
+                                                  // if n = 4: 3 * factorial(3) = 4 * 6 = 24
+    }
+  }
+
+  factorial(n);
+  solutionCount = result;
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
